f74d6988ee56e719d12e247ca6108dccd85662d1
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2020-09-14 13:29:08 +0000

    Support wine on linux

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const os = require('os');
 const Q = require('Q');
 const StreamZip = require('node-stream-zip');
 const url = require('url');
@@ -99,7 +100,7 @@ try {
 
           input.verbose && console.log(`Executing: ${command}`);
 
-          exec(command, error => {
+          exec(os.platform === 'win32' ? command : `wine ${command}`, error => {
             if (error && !fs.existsSync(input.logFilePath)) {
               throw new Error(error);
             }
