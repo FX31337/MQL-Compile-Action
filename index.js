0e662257a8585983a8f31526e79d20c0e76e1a25
kenorb <kenorb@users.noreply.github.com>
2024-02-04 16:45:25 +0000

    Do not execute main code on the post run

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ process.on('unhandledRejection', error => {
   core.setFailed(error ? error : 'Unknown error occurred!');
 });
 
+const isPost = Boolean(core.getState('isPost'));
 // Set this to false if you want to test the action locally via:
 // $ ncc build && node index.js
 const realRun = true;
@@ -50,156 +51,158 @@ if (realRun) {
   };
 }
 
-try {
-  fs.unlinkSync(input.logFilePath);
-} catch (e) {
-  // Silence the error.
-}
-
-try {
-  process.chdir(input.workingDirectory);
-
-  const configFilePath = `tester.ini`;
-  const terminal64Exe =
-    glob.sync(Path.join(input.platformPath, '**', 'terminal64.exe'))[0] || '';
-  const terminal32Exe =
-    glob.sync(Path.join(input.platformPath, '**', 'terminal.exe'))[0] || '';
-  const terminalExe = terminal64Exe || terminal32Exe || '';
-  const platformPath = Path.dirname(terminalExe) || '';
-  // Const platformPathAbs = Path.resolve(glob.sync(platformPath)[0]);
-
-  if (terminalExe.length > 0) {
-    input.verbose && console.log(`Terminal path: "${terminalExe}".`);
-  } else {
-    throw new Error(`Terminal cannot be found in "${input.platformPath}"!`);
+if (!isPost) {
+  try {
+    fs.unlinkSync(input.logFilePath);
+  } catch (e) {
+    // Silence the error.
   }
 
-  if (terminal32Exe.length > 0) {
-    fs.writeFileSync(
-      configFilePath,
-      '[Tester]\r\nShutdownTerminal=true\r\nTestExpert=Dummy\r\nTestShutdownTerminal=true\r\n'
-    );
-  }
-  const includePath =
-    input.includePath === ''
-      ? Path.join(platformPath, terminal64Exe.length > 0 ? 'MQL5' : 'MQL4')
-      : input.includePath;
+  try {
+    process.chdir(input.workingDirectory);
 
-  if (input.initPlatform) {
-    const command = `"${terminalExe}" /log:CON /portable ${
-      terminal64Exe.length > 0
-        ? `"/config:${configFilePath}"`
-        : `"${configFilePath}"`
-    }`;
+    const configFilePath = `tester.ini`;
+    const terminal64Exe =
+      glob.sync(Path.join(input.platformPath, '**', 'terminal64.exe'))[0] || '';
+    const terminal32Exe =
+      glob.sync(Path.join(input.platformPath, '**', 'terminal.exe'))[0] || '';
+    const terminalExe = terminal64Exe || terminal32Exe || '';
+    const platformPath = Path.dirname(terminalExe) || '';
+    // Const platformPathAbs = Path.resolve(glob.sync(platformPath)[0]);
 
-    input.verbose && console.log(`Executing: ${command}`);
+    if (terminalExe.length > 0) {
+      input.verbose && console.log(`Terminal path: "${terminalExe}".`);
+    } else {
+      throw new Error(`Terminal cannot be found in "${input.platformPath}"!`);
+    }
 
-    try {
-      const cmdOptions = { stdio: 'inherit',
-timeout: 20000 };
-      execSync(
-        os.platform() === 'win32' || isWsl ? command : `wine ${command}`,
-        cmdOptions
+    if (terminal32Exe.length > 0) {
+      fs.writeFileSync(
+        configFilePath,
+        '[Tester]\r\nShutdownTerminal=true\r\nTestExpert=Dummy\r\nTestShutdownTerminal=true\r\n'
       );
-      input.verbose && console.log('Initialization completed.');
-    } catch (e) {
-      // Silencing any error.
-      if (e.error) {
-        console.error(`Error: ${e.error}`);
-      }
     }
-  }
+    const includePath =
+      input.includePath === ''
+        ? Path.join(platformPath, terminal64Exe.length > 0 ? 'MQL5' : 'MQL4')
+        : input.includePath;
 
-  const checkSyntaxParam = input.checkSyntaxOnly ? '/s' : '';
+    if (input.initPlatform) {
+      const command = `"${terminalExe}" /log:CON /portable ${
+        terminal64Exe.length > 0
+          ? `"/config:${configFilePath}"`
+          : `"${configFilePath}"`
+      }`;
 
-  let files = [];
+      input.verbose && console.log(`Executing: ${command}`);
 
-  if (input.compilePath.includes('*'))
-    files = glob(input.compilePath, {
-      ignore: input.compilePathIgnore,
-      sync: true
-    });
-  else files = [input.compilePath];
-
-  input.verbose && console.log(`Files to compile: ${files}`);
-
-  if (input.includePath !== '' && !fs.existsSync(includePath))
-    throw new Error(`Directory at include path "${includePath}" not found!`);
-
-  // Compiling each file separately and checking log's output every time, as it always fresh.
-  for (const path of files) {
-    const cmdArgInc = input.includePath === '' ? '' : `/inc:"${includePath}"`;
-    const command = `"${terminalExe}" /portable ${cmdArgInc} /compile:"${path}" /log:"${input.logFilePath}" ${checkSyntaxParam}`;
-
-    input.verbose && console.log(`Executing: ${command}`);
-
-    try {
-      execSync(os.platform() === 'win32' || isWsl ? command : `wine ${command}`);
-    } catch (e) {
-      if (e.error) {
-        console.log(`Error: ${e.error}`);
+      try {
+        const cmdOptions = { stdio: 'inherit',
+timeout: 20000 };
+        execSync(
+          os.platform() === 'win32' || isWsl ? command : `wine ${command}`,
+          cmdOptions
+        );
+        input.verbose && console.log('Initialization completed.');
+      } catch (e) {
+        // Silencing any error.
+        if (e.error) {
+          console.error(`Error: ${e.error}`);
+        }
       }
     }
 
-    let log;
+    const checkSyntaxParam = input.checkSyntaxOnly ? '/s' : '';
 
-    try {
-      log = fs.readFileSync(input.logFilePath, 'utf-16le').toString('utf8');
-    } catch (e) {
-      console.log(`Missing log file "${input.logFilePath}".`);
-      log = 'No log file found.\n';
-    }
+    let files = [];
 
-    input.verbose && console.log('Log output:');
-    input.verbose && console.log(log);
+    if (input.compilePath.includes('*'))
+      files = glob(input.compilePath, {
+        ignore: input.compilePathIgnore,
+        sync: true
+      });
+    else files = [input.compilePath];
 
-    if (input.verbose && log.includes(`${platformPath}\\MQL`))
-      console.log(`Please check if you enabled "init-platform" for a MQL-Compile-Action as it looks like your code makes use of built-in MT's libraries!\n`);
+    input.verbose && console.log(`Files to compile: ${files}`);
 
-    const errorCheckingRuleWarning = /(.*warning [0-9]+:.*)/gu;
-    const errorCheckingRuleError = /(.*error [0-9]+:.*)/gu;
+    if (input.includePath !== '' && !fs.existsSync(includePath))
+      throw new Error(`Directory at include path "${includePath}" not found!`);
 
-    const wereErrorsFound = errorCheckingRuleError.test(log);
-    const wereWarningsFound =
-      !input.ignoreWarnings && errorCheckingRuleWarning.test(log);
+    // Compiling each file separately and checking log's output every time, as it always fresh.
+    for (const path of files) {
+      const cmdArgInc = input.includePath === '' ? '' : `/inc:"${includePath}"`;
+      const command = `"${terminalExe}" /portable ${cmdArgInc} /compile:"${path}" /log:"${input.logFilePath}" ${checkSyntaxParam}`;
 
-    if (wereErrorsFound || wereWarningsFound) {
-      input.verbose &&
-        console.log('Warnings/errors occurred. Returning exit code 1.');
+      input.verbose && console.log(`Executing: ${command}`);
 
-      // Composing error string.
-      let errorText = 'Compilation failed!';
-
-      const errors = [...log.matchAll(errorCheckingRuleError)];
-
-      if (errors.length > 0) {
-        errorText += '\n\nErrors found:\n';
-        for (const message of errors) {
-          errorText += `\n* ${message[0]}`;
+      try {
+        execSync(os.platform() === 'win32' || isWsl ? command : `wine ${command}`);
+      } catch (e) {
+        if (e.error) {
+          console.log(`Error: ${e.error}`);
         }
       }
 
-      const warnings = [...log.matchAll(errorCheckingRuleWarning)];
+      let log;
 
-      /* eslint max-depth: [2, 5] */
-      if (warnings.length > 0) {
-        if (input.ignoreWarnings)
-          errorText += '\n\n(Ignored) warnings found:\n';
-        else errorText += '\n\nWarnings found:\n';
-
-        for (const message of warnings) {
-          errorText += `\n* ${message[0]}`;
-        }
+      try {
+        log = fs.readFileSync(input.logFilePath, 'utf-16le').toString('utf8');
+      } catch (e) {
+        console.log(`Missing log file "${input.logFilePath}".`);
+        log = 'No log file found.\n';
       }
 
-      /* eslint-disable */
-      createComment(warnings, errors);
-      /* eslint-enable */
-      core.setFailed(errorText);
-    }
-  }
+      input.verbose && console.log('Log output:');
+      input.verbose && console.log(log);
 
-  input.verbose && console.log('Done.');
-} catch (error) {
-  core.setFailed(error.message);
+      if (input.verbose && log.includes(`${platformPath}\\MQL`))
+        console.log(`Please check if you enabled "init-platform" for a MQL-Compile-Action as it looks like your code makes use of built-in MT's libraries!\n`);
+
+      const errorCheckingRuleWarning = /(.*warning [0-9]+:.*)/gu;
+      const errorCheckingRuleError = /(.*error [0-9]+:.*)/gu;
+
+      const wereErrorsFound = errorCheckingRuleError.test(log);
+      const wereWarningsFound =
+        !input.ignoreWarnings && errorCheckingRuleWarning.test(log);
+
+      if (wereErrorsFound || wereWarningsFound) {
+        input.verbose &&
+          console.log('Warnings/errors occurred. Returning exit code 1.');
+
+        // Composing error string.
+        let errorText = 'Compilation failed!';
+
+        const errors = [...log.matchAll(errorCheckingRuleError)];
+
+        if (errors.length > 0) {
+          errorText += '\n\nErrors found:\n';
+          for (const message of errors) {
+            errorText += `\n* ${message[0]}`;
+          }
+        }
+
+        const warnings = [...log.matchAll(errorCheckingRuleWarning)];
+
+        /* eslint max-depth: [2, 6] */
+        if (warnings.length > 0) {
+          if (input.ignoreWarnings)
+            errorText += '\n\n(Ignored) warnings found:\n';
+          else errorText += '\n\nWarnings found:\n';
+
+          for (const message of warnings) {
+            errorText += `\n* ${message[0]}`;
+          }
+        }
+
+        /* eslint-disable */
+        createComment(warnings, errors);
+        /* eslint-enable */
+        core.setFailed(errorText);
+      }
+    }
+
+    input.verbose && console.log('Done.');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
